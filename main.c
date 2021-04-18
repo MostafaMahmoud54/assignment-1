4f5aa8cb5fef2de965895b5daed0abc2d31a9d94
Mostafa Mahmoud <abdelkader.mostafa33@gmail.com>
2021-04-19 00:43:55 +0200

    Developing a new structure

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+    char name[100];
+    int ID;
+    int day;
+    int month;
+    int year;
+    int score;
+};
+int main()
+{
+    printf("Welcome to Assignment 1!\n");
+    return 0;
+}
