cccbd477c2a62867c2902993165dc6914a916f62
lobna904 <82783466+lobna904@users.noreply.github.com>
2021-07-10 20:14:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Name:Ame Mousa              sec:3   bn21
+// Name:Lobna Ahmed Ismail     sec:3   bn:25
+// Name:Mostafa Mahmoud        sec:4   bn:20
+
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include <time.h>
+int n;
+struct student *s;
 struct student
 {
     char name[100];
@@ -9,63 +17,82 @@ struct student
     int year;
     int score;
 };
-int n;
-struct student *s;
+
 void insertarray()
-{          //insert the values of students
+{          //%%%%%%%
     for (int i=0;i<n;i++)
-   {printf("Enter the student (%d) name  ",i+1);    scanf("%*c");  fgets((s+i)->name,sizeof((s+i)->name),stdin);
-    printf("Enter the student (%d) id  ",i+1);      scanf ("%d", &(s+i)->id);
-    printf("Enter the student (%d) day  ",i+1);     scanf ("%d", &(s+i)->day);
-    printf("Enter the student (%d) month  ",i+1);   scanf ("%d", &(s+i)->month);
-    printf("Enter the student (%d) year  ",i+1);    scanf ("%d", &(s+i)->year);
-    printf("Enter the student (%d) score  ",i+1);   scanf ("%d", &(s+i)->score);
-    printf("#####DONE#####\n");
+   {printf("Write the Name of student number (%d) ",i+1);    scanf ("%s", (s+i)->name);
+    printf("Enter Id: ");                                    scanf ("%d", &(s+i)->id);
+    printf("Enter Last year score: ");                       scanf ("%d", &(s+i)->score);
+    printf("Enter Day of birth: ");                          scanf ("%d", &(s+i)->day);
+    printf("Enter Month: ");                                 scanf ("%d", &(s+i)->month);
+    printf("Enter Year: ");                                  scanf ("%d", &(s+i)->year);
+    printf("\n");
     }
 
 }
 void insertendarray()
-{      //insert student at the end of the array
-     n++;
-    s = (struct student*) realloc(s,n * sizeof(struct student));
-    printf("Enter the student name\n ");     scanf("%*c");  fgets((s+n-1)->name,sizeof((s+n-1)->name),stdin);
-    printf("Enter the student id\n");      scanf ("%d", &(s + n-1)->id);
-    printf("Enter the student day\n ");     scanf ("%d", &(s + n-1)->day);
-    printf("Enter the student month\n ");   scanf ("%d", &(s + n-1)->month);
-    printf("Enter the student year\n ");    scanf ("%d", &(s + n-1)->year);
-    printf("Enter the student score\n ");   scanf ("%d", &(s + n-1)->score);
+{       n++;        //&&&&&&&
+     clock_t tic=clock();
 
+    s = (struct student*) realloc(s,n * sizeof(struct student));
+    printf("Enter the student Name: ");    scanf ("%s", (s + n-1)->name);
+    printf("Enter ID: ");                  scanf ("%d", &(s + n-1)->id);
+    printf("Enter Last year score: ");     scanf ("%d", &(s + n-1)->score);
+    printf("Enter Day of birth: ");        scanf ("%d", &(s + n-1)->day);
+    printf("Enter Month: ");               scanf ("%d", &(s + n-1)->month);
+    printf("Enter Year: ");                scanf ("%d", &(s + n-1)->year);
+    printf("\n");
+
+    clock_t toc=clock();
+    printf("time execution %f seconds\n\n",(double)(toc-tic)/CLOCKS_PER_SEC);
 }
 void insertmiddlearray()
-{     //insert student at middle of the array
-    n++;
+{   n++;
+    clock_t tic=clock();
+
    s = (struct student*) realloc(s,n * sizeof(struct student));
    t:
     printf ("Insert the student index that you want to insert in = ");
     int k;
     scanf("%d", &k);
-    if(k>n-1)
+    printf("\n");
+    if(k>n || k==0)
 
-      { printf("Can not insert in index greater than %d\n",n-1);
+      { printf("Enter index from 1 to %d\n",n);
         goto t;}
-    else
-     {
-     for(int j=n-1;j>k-1;j--)
+    else if(k==1)
+        {
+            insertbeginning(); n--;
+            return;
+        }
+    else if(k==n-1)
+    {
+        insertendarray();
+        return;
+    }
+        else
+     {for(int j=n-1;j>k-1;j--)
          {
              s[j]=s[j-1];
          }
-    printf("Enter the student name\n ");    scanf("%*c");  fgets((s+k)->name,sizeof((s+k)->name),stdin);
-    printf("Enter the student id\n ");      scanf ("%d", &(s + k)->id);
-    printf("Enter the student day\n ");     scanf ("%d", &(s + k)->day);
-    printf("Enter the student month\n ");   scanf ("%d", &(s + k)->month);
-    printf("Enter the student year\n ");    scanf ("%d", &(s + k)->year);
-    printf("Enter the student score\n ");   scanf ("%d", &(s + k)->score); }
-
+    printf("Enter the student Name: ");    scanf ("%s", (s + k-1)->name);
+    printf("Enter ID: ");                  scanf ("%d", &(s + k-1)->id);
+    printf("Enter Last year score: ");     scanf ("%d", &(s + k-1)->score);
+    printf("Enter Day of birth: ");        scanf ("%d", &(s + k-1)->day);
+    printf("Enter Month: ");               scanf ("%d", &(s + k-1)->month);
+    printf("Enter Year: ");                scanf ("%d", &(s + k-1)->year);
+    printf("\n");
+     }
+    clock_t toc=clock();
+    printf("time execution %f seconds\n\n",(double)(toc-tic)/CLOCKS_PER_SEC);
 }
 
 void insertbeginning()
-{  //insert in the beginning of the array
+{
  n++;
+    clock_t tic=clock();
+
    s = (struct student*) realloc(s,n * sizeof(struct student));
 
     for(int i=n-1;i>0;i--)
@@ -73,50 +100,60 @@ void insertbeginning()
         s[i]=s[i-1];
 
     }
-         printf("Enter the student name\n ");  scanf("%*c");  fgets((s+0)->name,sizeof((s+0)->name),stdin);
-         printf("Enter the student id\n ");    scanf ("%d", &(s + 0)->id);
-         printf("Enter the student day\n ");   scanf ("%d", &(s + 0)->day);
-         printf("Enter the student month\n "); scanf ("%d", &(s + 0)->month);
-         printf("Enter the student year\n ");  scanf ("%d", &(s + 0)->year);
-         printf("Enter the student score\n "); scanf ("%d", &(s + 0)->score);
-         }
+    printf("Enter the student Name: ");    scanf ("%s", (s + 0)->name);
+    printf("Enter ID: ");                  scanf ("%d", &(s + 0)->id);
+    printf("Enter Last year score: ");     scanf ("%d", &(s + 0)->score);
+    printf("Enter Day of birth: ");        scanf ("%d", &(s + 0)->day);
+    printf("Enter Month: ");               scanf ("%d", &(s + 0)->month);
+    printf("Enter Year: ");                scanf ("%d", &(s + 0)->year);
+    printf("\n");
+
+    clock_t toc=clock();
+    printf("time execution %f seconds\n\n",(double)(toc-tic)/CLOCKS_PER_SEC);
+
+}
 
 void print()
-{  //print function to print the final change that has happened in the array
+{
     if(n==0)
-        printf("The list is empty");
+        printf("The list is empty \n");
     for (int i=0;i<n;i++)
     {
-        printf ("Name of Student number (%d) : %s",i+1, (s+i)->name);
-        printf ("id : %d\n", (s+i)->id);
-        printf ("day of birth : %d\n", (s+i)->day);
-        printf ("month : %d\n", (s+i)->month);
-        printf ("year : %d\n", (s+i)->year);
-        printf ("Last year score : %d\n", (s+i)->score);
+        printf ("Name of Student number (%d) is %s\n",i+1,(s+i)->name);
+        printf ("Id %d\n",(s+i)->id);
+        printf ("Last year score %d\n",(s+i)->score);
+        printf ("Day of birth %d\n",(s+i)->day);
+        printf ("Month %d\n",(s+i)->month);
+        printf ("Year %d\n",(s+i)->year);
+        printf("\n");
+          //Printing values in array of structures
     }
 }
 
-int main()
+int main ()
 {
-    printf("Welcome to Assignment 1!\n");
-     printf("Enter the number of students: ");
+    printf("welcome to our project \n");
+    printf("Enter the number of students: ");
     scanf ("%d", &n);
 
-    s = (struct student*) malloc(n * sizeof(struct student));
-    insertarray(); print();
+    s = (struct student*) malloc(n * sizeof(struct student)); // Initializing pointer to a structure
+    insertarray();                                   //Creating memory for array of structures dynamically
 
  y:
- printf("\n How can i help you? \nEnter 1 to insert a new student in the beginning \n 2 to insert in the middle \n 3 to insert at the end \n 4 to print \n if you want to exit press 5 \n");
+ printf("Enter 1 to insert a new student in the beginning \n Enter 2 to insert in the middle \n Enter 3 to insert at the end \n Enter 4 to print \n if you want to exit press 5 \n ");
 
  int k;
  scanf("%d",&k);
- switch(k)     //switch case to make the user choose from
+ switch(k)
  {
-   case 1: insertbeginning();   goto y; //operation to insert a new student in the beginning
-   case 2: insertmiddlearray(); goto y; //operation to insert a new student in the middle
-   case 3: insertendarray();    goto y; //operation to insert a new student in the end
+   case 1: insertbeginning();   goto y;
+   case 2: insertmiddlearray(); goto y;
+   case 3: insertendarray();    goto y;
    case 4: print(); goto y;
    case 5: break;
  }
-    return 0;
-}
+
+  printf(" Size of structure %d\n",sizeof(struct student));   //to calculate the size of structure
+
+    return 0;}
+
